e69b62d34148e1dde0c97604c20ca2c44b2ee895
Oriol Ferrer Mesia <oriol@uri.cat>
2017-04-21 23:53:16 -0400

    wip 2 button and led setup

--- TOM___IR___send/TOM___IR___send.ino
+++ TOM___IR___send/TOM___IR___send.ino
@@ -12,29 +12,50 @@
 // class called "IRsend" containing only the protocols you want.
 // Now declare an instance of that sender.
 
+const int BUTTON1_PIN = 5;
+const int BUTTON2_PIN = 6;
+
+const int BUTTON1_LED = 7;
+const int BUTTON2_LED = 8;
+
 IRsend mySender;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(5, INPUT);
+  pinMode(BUTTON1_PIN , INPUT);
+  pinMode(BUTTON2_PIN , INPUT);
+
+  pinMode(BUTTON1_LED , OUTPUT);
+  pinMode(BUTTON2_LED , OUTPUT);
+
   delay(2000); while (!Serial); //delay for Leonardo
   Serial.println(F("Every time you press a key is a serial monitor we will send."));
 }
 
 void loop() {
-  //if (Serial.read() != -1) {
-    //send a code every time a character is received from the 
-    // serial port. You could modify this sketch to send when you
-    // push a button connected to an digital input pin.
-    //Substitute values and protocols in the following statement
-    // for device you have available.
-if (digitalRead(5) == LOW){
+  
+  if (digitalRead(BUTTON1_PIN) == LOW){
     mySender.send(SONY,0xa8bca, 20);//Sony DVD power A8BCA, 20 bits
-    //mySender.send(NEC,0x61a0f00f,0);//NEC TV power button=0x61a0f00f
-    delay(1000);
-    Serial.println(F("Sent signal."));
-}
-    Serial.println("Nothing");
+    Serial.println(F("Button1 Pressed"));
+    analogWrite(BUTTON1_LED , 100);
+  }else{
+    analogWrite(BUTTON1_LED , 0);
+  }
+
+  int v = analogRead(BUTTON2_PIN);
+  Serial.println(v);
+  if (v < 10 ){
+    mySender.send(SONY,0xa8bca, 20);//Sony DVD power A8BCA, 20 bits
+    Serial.println(F("Button2 Pressed"));
+    analogWrite(BUTTON2_LED , 100);
+  }else{
+    analogWrite(BUTTON2_LED , 0);
+    Serial.println(F("Button2 NOT Pressed"));
+  }
+
+  delay(500);
+
+  //Serial.println("Nothing");
   //}
 }
 
